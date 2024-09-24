85d7c7a5cbe06a50510be50cb0eb390caa9ed6e0
EnriqueBDL <131392738+EnriqueBDeL@users.noreply.github.com>
2024-09-24 16:05:21 +0200

    Add files via upload

---
+++ Ej 1 Bol 1 Repaso.c
@@ -0,0 +1,50 @@
+// EJERCCIO 1
+
+
+#include <stdio.h>
+#include "stdlib.h"
+
+int main(int argc, char *argv[]){
+
+printf("El clásico \"Hola Mundo\"\n");
+
+system("PAUSE");
+
+return 0;
+}
+
+
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+// 1. Sirve para parar de compilar, hasta que el usurio presione enter para continuar.
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+/* 2.
+    stdio.h contiene las funciones para trabajar con los ficheros.
+    stdlib.h contiene lo necesario para usar memoria dinamica, entre otras cosas.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+/* 3.
+    #include <librería>: Inclulle en el codigo el contenido que hay dentro de las librerias. Se utiliza para activar sentencias  con funciones en concreto.
+    #include “librería”: Inclulle archivos tipo ".c" o ".h" a nuestro codigo. Se utiliza para trabajar con multiples codigos como si fueran uno.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+/* 4.
+    argc:indica el número de argumentos que se pasan al programa.
+    argv:contiene cada uno de los argumentos pasados al programa.
+
+
+    No es necesario utilizar los dos.
+*/
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+/* 5.
+
+   Termina en "return 0;" pora así representar que acabó la ejecución correctamente.
+
+   Si utilizas un return con otro numero que no sea 0, es que a ocurrido un error durente el proceso.
+
+*/
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
+
+
+
+
+
